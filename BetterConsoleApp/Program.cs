c5b3c1363b93c81b12ca854abdcaf942e67e3db2
JeePee <jpt@sultancrm.nl>
2022-11-17 13:21:00 +0100

    License

--- BetterConsoleApp/Program.cs
+++ BetterConsoleApp/Program.cs
@@ -1,11 +1,40 @@
-﻿using BetterConsoleApp;
+﻿#region MIT License
+
+// ==========================================================
+// 
+// ConsoleWithLogging project - Copyright (c) 2022 JeePeeTee
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+// ===========================================================
+
+#endregion
+
+#region usings
+
+using BetterConsoleApp;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Exceptions;
-using Serilog.Formatting.Json;
-using Serilog.Sinks.File;
+
+#endregion
 
 var builder = new ConfigurationBuilder();
 BuildConfig(builder);
@@ -14,7 +43,7 @@ Log.Logger = new LoggerConfiguration()
     // Moved to appsettings.json
     //.Enrich.FromLogContext()
     //.Enrich.WithExceptionDetails( )
-   
+
     //.WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM-yyyy HH:mm:ss.fff} {Level:u3}] {Message:lj}{NewLine}{Exception}")
     //.WriteTo.Sink(new FileSink(
     //         @"C:\logs",
@@ -27,9 +56,7 @@ Log.Logger = new LoggerConfiguration()
 Log.Logger.Information("Application starting...");
 
 var host = Host.CreateDefaultBuilder()
-    .ConfigureServices((contect, services) => {
-        services.AddTransient<IGreetingService, GreetingService>();
-    })
+    .ConfigureServices((contect, services) => { services.AddTransient<IGreetingService, GreetingService>(); })
     .UseSerilog()
     .Build();
 
